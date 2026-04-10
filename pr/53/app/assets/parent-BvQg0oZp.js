21eb12e734a7db7a336c55d6f96d427919a34895
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-10 23:36:28 +0000

    deploy: PR #53 preview (0.2.0-pr.53)

---
+++ pr/53/app/assets/parent-BvQg0oZp.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-DtsMKoin.js";const e=()=>t.jsx("div",{children:t.jsx(o,{})});export{e as component};
---
+++ pr/53/docs/assets/dropdown-menu.stories-Ckp4xacL.js
@@ -0,0 +1,13 @@
+import{j as o}from"./iframe-DI3U9ZE4.js";import{B as t}from"./button-CZZv7X5k.js";import{D as e,a as p,b as s,c as r,d as i}from"./dropdown-menu-BO_8u9l6.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-BQHNewu7.js";import"./index-CJ93iEdS.js";import"./index-DJaNHGL6.js";import"./index-Vjd23Ahm.js";import"./index-Cxa3ir9o.js";import"./index-CSezrM5c.js";import"./index-Cs4dCDBu.js";import"./index-QCn6MONe.js";import"./tslib.es6--Hu8dhvm.js";import"./index-CwZ_KGn1.js";import"./index-B0siB64w.js";const C={component:e,tags:["autodocs"]},n={render:()=>o.jsxs(e,{children:[o.jsx(p,{asChild:!0,children:o.jsx(t,{variant:"outline",children:"Options"})}),o.jsxs(s,{children:[o.jsx(r,{children:"Profile"}),o.jsx(r,{children:"Settings"}),o.jsx(i,{}),o.jsx(r,{children:"Logout"})]})]})};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: () => <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Options</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+}`,...n.parameters?.docs?.source}}};const S=["Default"];export{n as Default,S as __namedExportsOrder,C as default};
