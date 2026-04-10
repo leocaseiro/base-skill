d24b17a99abe137c0912cb512cd765afa215f8ed
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-10 03:09:57 +0000

    deploy: production 0.2.0

---
+++ assets/parent-Df53y23z.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-B6zJMLHx.js";const e=()=>t.jsx("div",{children:t.jsx(o,{})});export{e as component};
---
+++ docs/assets/label.stories-BzSjJQv9.js
@@ -0,0 +1,6 @@
+import{j as r}from"./iframe-CqmSs6WP.js";import{I as o}from"./input-BcsSkDU0.js";import{L as a}from"./label-C_DcsAZK.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-BQHNewu7.js";import"./index-Q5Mtw0QX.js";import"./index-H8mH5EvO.js";import"./index-pTK7p3TG.js";import"./index-BQDxU-BG.js";const u={component:a,tags:["autodocs"]},e={render:()=>r.jsxs("div",{className:"flex flex-col gap-2",children:[r.jsx(a,{htmlFor:"name",children:"Name"}),r.jsx(o,{id:"name",placeholder:"Your name"})]})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2">
+      <Label htmlFor="name">Name</Label>
+      <Input id="name" placeholder="Your name" />
+    </div>
+}`,...e.parameters?.docs?.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,u as default};
