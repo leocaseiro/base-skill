42d912c663f34f0f80f32101e5500d77e0ad0c48
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-11 19:16:54 +0000

    deploy: PR #81 preview (0.2.0-pr.81)

---
+++ pr/81/app/assets/route-Du-MsUUs.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-B_j9TIIh.js";import{i as n}from"./i18n-8Z2UCN8f.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
