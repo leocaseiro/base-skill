9dbf15bacd89a3b9f74e505fd5406a783076c6a8
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-11 18:03:13 +0000

    deploy: PR #80 preview (0.2.0-pr.80)

---
+++ pr/80/app/assets/_gameId-xsRO-j7u.js
@@ -0,0 +1,1 @@
+import{aa as t,j as a,ab as e}from"./main-Dtde_m8D.js";const n=()=>{const o=t.useLoaderData();return a.jsx(e,{...o})};export{n as component};
---
+++ pr/80/app/assets/parent-CyOPiFDT.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-Dtde_m8D.js";const e=()=>t.jsx("div",{children:t.jsx(o,{})});export{e as component};
