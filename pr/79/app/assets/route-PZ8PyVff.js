1a524758c7d2c94997eca15c014a9f867ee62bca
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-11 18:02:37 +0000

    deploy: PR #79 preview (0.2.0-pr.79)

---
+++ pr/79/app/assets/route-PZ8PyVff.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-DerdEVL9.js";import{i as n}from"./i18n-bWMGIo2t.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
