9a9cfba3d0b023fe95134b279714fa0cf2bae031
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-11 18:12:12 +0000

    deploy: production 0.3.0

--- assets/route-Bk-UjIAn.js
+++
@@ -1,1 +0,0 @@
-import{R as e,r as o,j as a,O as s}from"./main-CkV2uwnJ.js";import{i as n}from"./i18n-D-B9U-0_.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
