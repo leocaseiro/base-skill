a616188ea13ca0f95d9618357d0d94def424a982
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-14 05:43:39 +0000

    deploy: PR #94 preview (0.2.0-pr.94)

---
+++ pr/94/app/assets/_gameId-C7Ti1xEW.js
@@ -0,0 +1,1 @@
+import{ab as t,j as a,ac as e}from"./main-Bg0A_who.js";const n=()=>{const o=t.useLoaderData();return a.jsx(e,{...o})};export{n as component};
---
+++ pr/94/app/assets/route-CLyXDFbz.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-Bg0A_who.js";import{i as n}from"./i18n-mBugBGva.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
