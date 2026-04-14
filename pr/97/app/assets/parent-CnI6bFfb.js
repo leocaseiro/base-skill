1b21ec634adda9a4cdb9ad6900dfc5debc43b58a
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-14 14:28:22 +0000

    deploy: PR #97 preview (0.3.0-pr.97)

---
+++ pr/97/app/assets/parent-CnI6bFfb.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-CrempDpr.js";const e=()=>t.jsx("div",{children:t.jsx(o,{})});export{e as component};
---
+++ pr/97/docs/assets/label.stories-Brnzhk6_.js
@@ -0,0 +1,6 @@
+import{j as r}from"./iframe-DYB_duEN.js";import{I as o}from"./input-DVaytroB.js";import{L as a}from"./label-xEAbC5kO.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-BQHNewu7.js";import"./index-BZlGGW4L.js";import"./index-J01gOvXZ.js";import"./index-lT3Fg137.js";import"./index-DzN_gxQi.js";const u={component:a,tags:["autodocs"]},e={render:()=>r.jsxs("div",{className:"flex flex-col gap-2",children:[r.jsx(a,{htmlFor:"name",children:"Name"}),r.jsx(o,{id:"name",placeholder:"Your name"})]})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2">
+      <Label htmlFor="name">Name</Label>
+      <Input id="name" placeholder="Your name" />
+    </div>
+}`,...e.parameters?.docs?.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,u as default};
