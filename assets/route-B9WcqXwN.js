21ab06356d8f0c210812cc0ce98842a127cf09a1
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-21 20:22:17 +0000

    deploy: production 0.12.2

---
+++ assets/route-B9WcqXwN.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-qf6pvYiU.js";import{i as n}from"./i18n-gTrn7SBZ.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
