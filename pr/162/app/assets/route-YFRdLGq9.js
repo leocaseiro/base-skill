9f9464ced2dfa56b65fdfffd22306c2bd1bd777c
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-22 08:56:57 +0000

    deploy: PR #162 preview (0.13.1-pr.162)

---
+++ pr/162/app/assets/route-YFRdLGq9.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-BP1KO8pR.js";import{i as n}from"./i18n-D3jBrOF-.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
--- pr/162/app/assets/version-BICQ4CXb.js
+++
@@ -1,1 +0,0 @@
-const o="0.13.0-pr.162";export{o as A};
