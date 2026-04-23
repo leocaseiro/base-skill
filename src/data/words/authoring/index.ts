30e9a11e1f3e07092f6fb4f33d49eef91a8e1a0a
leocaseiro <leobok@gmail.com>
2026-04-24 05:36:29 +1000

    feat(word-authoring): add rita + zod deps, scaffold authoring folder

---
+++ src/data/words/authoring/index.ts
@@ -0,0 +1,7 @@
+export { generateBreakdown } from './engine';
+export type { Breakdown } from './engine';
+export { align } from './aligner';
+export type { AlignedGrapheme } from './aligner';
+export { draftStore } from './draftStore';
+export { AuthoringPanel } from './AuthoringPanel';
+export { DraftsPanel } from './DraftsPanel';
