96f005e4d71a27551671208ad7365ccf23b37b74
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-28 12:07:34 +0000

    deploy: production 0.17.4

--- docs/assets/index-PgAsUCXN.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-CJ91jCJK.js";import{r as o}from"./index-BxpgkfM_.js";var t=o();const m=r(t);export{m as R,t as r};
--- docs/assets/index-orv1Au3Z.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-COa2pYd-.js";var i=r();export{i as s};
