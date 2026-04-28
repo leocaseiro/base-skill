d1f9766d04b0630976f8eaf15a704eba02adc42f
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-28 05:45:52 +0000

    deploy: PR #205 preview (0.17.2-pr.205)

---
+++ pr/205/docs/assets/NumeralTileBank.stories-83toLHkq.js
@@ -0,0 +1,36 @@
+import{j as e}from"./iframe-Myy5bJzj.js";import{w as l}from"./withDb-B8W6YqPE.js";import{N as S}from"./NumeralTileBank-BwyfEvx5.js";import{A as d,u as c}from"./AnswerGameProvider-Ck_jmPAx.js";import"./preload-helper-PPVm8Dsz.js";import"./DbProvider-CIKyhp0p.js";import"./index.browser-BY9c7rfI.js";import"./Subject-D3vFcUgp.js";import"./tslib.es6--Hu8dhvm.js";import"./useDraggableTile-CQVWXTHp.js";import"./useGameTTS-7N4QiWCf.js";import"./useSettings-Bh5QHgaV.js";import"./useRxDB-DkBdH6GZ.js";import"./useRxQuery-BkcvEkIK.js";import"./SpeechOutput-Dyw1q0Rq.js";import"./safe-get-voices-dEfMv828.js";import"./useTranslation-DJYp6Tek.js";import"./index-fyFnyQZ7.js";import"./index-BKWzJUyU.js";import"./tile-font-DQ9RrPM_.js";import"./AudioFeedback-DxUgBcwr.js";const p={gameId:"storybook",inputMethod:"drag",wrongTileBehavior:"lock-auto-eject",tileBankMode:"exact",totalRounds:1,ttsEnabled:!0},m=[{id:"t1",label:"1",value:"1"},{id:"t2",label:"2",value:"2"},{id:"t3",label:"3",value:"3"},{id:"t4",label:"4",value:"4"},{id:"t5",label:"5",value:"5"}],g=[{id:"w1",label:"one",value:"1"},{id:"w2",label:"two",value:"2"},{id:"w3",label:"three",value:"3"},{id:"w4",label:"seventeen",value:"17"},{id:"w5",label:"twenty-two",value:"22"}],u=[{id:"z0",index:0,expectedValue:"3",placedTileId:null,isWrong:!1,isLocked:!1}],w=({children:r})=>(c()({type:"INIT_ROUND",tiles:m,zones:u}),e.jsx(e.Fragment,{children:r})),h=({children:r})=>(c()({type:"INIT_ROUND",tiles:g,zones:u}),e.jsx(e.Fragment,{children:r})),z={component:S,tags:["autodocs"],args:{tileStyle:"dots",tilesShowGroup:!0},decorators:[l,r=>e.jsx(d,{config:p,children:e.jsx(w,{children:e.jsx(r,{})})})]},o={args:{tileStyle:"dots",tilesShowGroup:!0}},s={args:{tileStyle:"fingers",tilesShowGroup:!0}},a={args:{tileStyle:"objects",tilesShowGroup:!0}},i={args:{tileStyle:"dots",tilesShowGroup:!1},decorators:[l,r=>e.jsx(d,{config:p,children:e.jsx(h,{children:e.jsx(r,{})})})]},y=({children:r})=>{const t=c();return t({type:"INIT_ROUND",tiles:m,zones:u}),t({type:"PLACE_TILE",tileId:"t1",zoneIndex:0}),t({type:"SET_DRAG_ACTIVE",tileId:"t1"}),t({type:"SET_DRAG_HOVER_BANK",tileId:"t2"}),e.jsx(e.Fragment,{children:r})},n={args:{tileStyle:"dots",tilesShowGroup:!0},decorators:[l,r=>e.jsx(d,{config:p,children:e.jsx(y,{children:e.jsx(r,{})})})]};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    tileStyle: 'dots',
+    tilesShowGroup: true
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    tileStyle: 'fingers',
+    tilesShowGroup: true
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    tileStyle: 'objects',
+    tilesShowGroup: true
+  }
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    tileStyle: 'dots',
+    tilesShowGroup: false
+  },
+  decorators: [withDb, Story => <AnswerGameProvider config={config}>
+        <InitWordProvider>
+          <Story />
+        </InitWordProvider>
+      </AnswerGameProvider>]
+}`,...i.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    tileStyle: 'dots',
+    tilesShowGroup: true
+  },
+  decorators: [withDb, Story => <AnswerGameProvider config={config}>
+        <DragHoverBankTileSetup>
+          <Story />
+        </DragHoverBankTileSetup>
+      </AnswerGameProvider>]
+}`,...n.parameters?.docs?.source}}};const L=["Default","FingersStyle","ObjectsStyle","WordTiles","DragHoverBankTile"];export{o as Default,n as DragHoverBankTile,s as FingersStyle,a as ObjectsStyle,i as WordTiles,L as __namedExportsOrder,z as default};
---
+++ pr/205/docs/assets/index-D8zX6KoB.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Myy5bJzj.js";import{r as o}from"./index-XXdl-_G7.js";var t=o();const m=r(t);export{m as R,t as r};
