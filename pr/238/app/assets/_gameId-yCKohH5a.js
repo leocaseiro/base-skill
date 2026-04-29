353741fcd4041e07dde0a51d0a7f658f20aa3b9d
leocaseiro <leocaseiro@users.noreply.github.com>
2026-04-29 11:44:19 +0000

    deploy: PR #238 preview (0.18.0-pr.238)

--- pr/238/app/assets/_gameId-yCKohH5a.js
+++
@@ -1,1 +0,0 @@
-import{aa as e,j as o,ab as s}from"./main-Bg9C6exB.js";const u=()=>{const t=e.useLoaderData(),a=e.useSearch();return o.jsx(s,{...t,debug:a.debug===!0})};export{u as component};
--- pr/238/app/assets/parent-DHvhbulZ.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./main-Bg9C6exB.js";const e=()=>t.jsx("div",{children:t.jsx(o,{})});export{e as component};
