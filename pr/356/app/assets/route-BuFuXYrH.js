f365bc6d8552b54c517da3f7a067caeb2efbb94d
leocaseiro <leocaseiro@users.noreply.github.com>
2026-05-11 12:25:09 +0000

    deploy: PR #356 preview (0.33.0-pr.356)

---
+++ pr/356/app/assets/route-BuFuXYrH.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,j as a,O as s}from"./main-v4S_UhlV.js";import{i as n}from"./i18n-D4hhcEKu.js";const m=()=>{const{locale:t}=e.useParams();return n.changeLanguage(t),o.useEffect(()=>{document.documentElement.lang=t==="pt-BR"?"pt-BR":"en"},[t]),a.jsx(s,{})};export{m as component};
---
+++ pr/356/docs/assets/channel-012GW7c_.js
@@ -0,0 +1,1 @@
+import{U as a,D as n}from"./mermaid.core-CREQL7aa.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
